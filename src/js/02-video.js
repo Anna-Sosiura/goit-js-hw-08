2ee5cb91742f6ced953a14f6b8c20da45325c0cc
Anna-Sosiura <132984503+Anna-Sosiura@users.noreply.github.com>
2023-09-22 00:35:58 +0300

    goit-js-hw-08(6)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,22 +2,25 @@ import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
 
     player.on('timeupdate', throttle(setTime, 1000));
-    function setTime(currentTime){
-        localStorage.setItem('videoplayer-current-time',JSON.stringify(currentTime));}
-       const savedSecond= localStorage.getItem('videoplayer-current-time');
+    const savedSecond= localStorage.getItem('videoplayer-current-time');
             const secondsValue=(JSON.parse(savedSecond));
 
+    if (savedSecond!==null){
         player.setCurrentTime(secondsValue.seconds).then(function(seconds) {
             // seconds = the actual time that the player seeked to
         }).catch(function(error) {
-            switch (error.name) {
-                case 'RangeError':
-                    // the time was less than 0 or greater than the video’s duration
-                    break;
-        
-                default:
-                    // some other error occurred
-                    break;
-            }
-        });
- 
+                switch (error.name) {
+                    case 'RangeError':
+                        // the time was less than 0 or greater than the video’s duration
+                        break;
+            
+                    default:
+                        // some other error occurred
+                        break;
+                }
+            });
+    }
+
+    function setTime(currentTime){
+        localStorage.setItem('videoplayer-current-time',JSON.stringify(currentTime));}
+    
